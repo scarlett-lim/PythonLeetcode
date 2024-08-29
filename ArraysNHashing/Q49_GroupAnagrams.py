5430e6f7541dd52da084cda6ce295e9ab39a8e92
scarlett-lim <appleladada@gmail.com>
2024-08-29 21:31:07 +0800

    revise q49

--- ArraysNHashing/Q49_GroupAnagrams.py
+++ ArraysNHashing/Q49_GroupAnagrams.py
@@ -5,15 +5,33 @@ from typing import List
 class Solution:
     # Time Spcae O(m*n)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list) # handle edge case to make the default dict to list
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c)-ord("a")] +=1
-            # put tuple bcuz list cnt be key in dictionary
-            res[tuple(count)].append(s)
+        dictionary = defaultdict(list)
 
-        return res.values()
+        for word in strs:
+            count = [0] * 26
+            for char in word:
+                count[ord(char) - ord("a")] += 1
+            dictionary[tuple(count)].append(word)
+
+        return dictionary.values()
+
+
+
+
+
+
+
+
+
+        # res = defaultdict(list) # handle edge case to make the default dict to list
+        # for s in strs:
+        #     count = [0] * 26
+        #     for c in s:
+        #         count[ord(c)-ord("a")] +=1
+        #     # put tuple bcuz list cnt be key in dictionary
+        #     res[tuple(count)].append(s)
+        #
+        # return res.values()
 
 
 
@@ -39,7 +57,7 @@ def run_tests():
     assert result3 == expected3, f"Test case 3 failed: expected {expected3}, got {result3}"
 
     # Add more test cases as needed
-    print("Q49 All test cases passed!")
+    print("All Q49 test cases passed!")
 
 
 if __name__ == "__main__":
